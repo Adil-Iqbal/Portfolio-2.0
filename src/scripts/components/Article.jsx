f978e3533e7005228e9f6bd33ce03bd43bfc82fe
Adil-Iqbal <aiqbal85@gmail.com>
2019-08-15 22:05:52 -0500

    Article abstraction

--- src/scripts/components/Article.jsx
+++ src/scripts/components/Article.jsx
@@ -1,31 +1,34 @@
-/* eslint-disable max-len */
+import PropTypes from 'prop-types';
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+import ButtonToolbar from 'react-bootstrap/ButtonToolbar';
 import Card from 'react-bootstrap/Card';
 import Image from 'react-bootstrap/Image';
-import ButtonToolbar from 'react-bootstrap/ButtonToolbar';
-import Button from 'react-bootstrap/Button';
 import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
 import Popover from 'react-bootstrap/Popover';
 
-function Article() {
+function Article(props) {
+  const {
+    image, title, about, uuid, author, publishDate, tags, readURL,
+  } = props;
   return (
     <Card border="secondary" style={{ width: '100%' }} className="my-3">
       <Card.Body className="p-1">
-        <Image src="https://via.placeholder.com/120" className="float-left mr-3" />
-        <Card.Title>Project Euler - Problem 15 - Solution Explained</Card.Title>
-        <Card.Text>We explore the notorious Lattice paths problem. We use permutations to help model our solution; and combinations to optimize.</Card.Text>
+        <Image src={image} className="float-left mr-3" />
+        <Card.Title>{title}</Card.Title>
+        <Card.Text>{about}</Card.Text>
         <ButtonToolbar className="d-flex justify-content-end">
           <OverlayTrigger
             placement="left"
             overlay={(
-              <Popover id="239823">
+              <Popover id={uuid}>
                 <Popover.Content>
                   {'Written by '}
-                  <b>Adil Iqbal</b>
-                  {' on '}
-                  <b>April 22, 1993</b>
+                  <b>{author}</b>
+                  {' and published on '}
+                  <b>{publishDate}</b>
                   {'. (Tags: '}
-                  <i>Project Euler, Javascript</i>
+                  <i>{tags.join(', ')}</i>
                   {')'}
                 </Popover.Content>
               </Popover>
@@ -35,11 +38,22 @@ function Article() {
               <Button variant="link" className="mr-3" disabled>Info</Button>
             </span>
           </OverlayTrigger>
-          <Button variant="primary" className="mr-3">Read</Button>
+          <Button variant="primary" className="mr-3" onClick={() => window.open(readURL, '_blank')}>Read</Button>
         </ButtonToolbar>
       </Card.Body>
     </Card>
   );
 }
+
+Article.propTypes = {
+  image: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  about: PropTypes.string.isRequired,
+  uuid: PropTypes.string.isRequired,
+  author: PropTypes.string.isRequired,
+  publishDate: PropTypes.string.isRequired,
+  tags: PropTypes.arrayOf(PropTypes.string).isRequired,
+  readURL: PropTypes.string.isRequired,
+};
 
 export { Article as default };
